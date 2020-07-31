065c58e7a92111295927239d0ea5f05254b7d992
walsidalw <kleinfachnull@gmail.com>
2020-07-31 22:58:59 +0200

    Reworked empty output handling for queries.

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ with open("test_config.yaml", "r") as ymlfile:
     cfg = yaml.safe_load(ymlfile)
 
 orgaId = (cfg['opencast'])['organizationId']
+rp = (cfg['influxdb'])['retention-policy']
 
 """ Initializing clients """
 oc_client = occlient.OcClient(cfg['opencast'])
@@ -23,7 +24,8 @@ def index():
 
 @app.route('/org')
 def org():
-    return render_template('org.html')
+    df = influxclient.get_views(influx_clients['df_client'], rp, 'impressions_daily', 'organizationId', orgaId, orgaId)
+    return render_template('org.html', tables=[df.to_html(classes='data')], titles=df.columns.values)
 
 
 @app.route('/series')
@@ -36,15 +38,19 @@ def series():
 def series_details(series_id):
     series_name = oc_client.get_series_name(series_id)
     episodes = oc_client.get_all_episodes(series_id)
+    df = influxclient.get_views(influx_clients['df_client'], rp, 'impressions_daily', 'seriesId', series_id, orgaId)
     return render_template('series_details.html', series_id=series_id, series_name=series_name,
-                           episodes=episodes)
+                           episodes=episodes, tables=[df.to_html(classes='data')], titles=df.columns.values)
 
 
 @app.route('/episodes/<episode_id>')
 def episode_details(episode_id):
     episode_data = oc_client.get_episode_data(episode_id)
+    df = influxclient.get_views(influx_clients['df_client'], rp, 'impressions_daily', 'eventId', episode_id, orgaId)
+    seg = influxclient.get_segments(influx_clients['point_client'], rp, 'segments_daily', episode_id, orgaId)
     return render_template('episode_details.html', episode_name=episode_data[0], series_id=episode_data[1],
-                           series_name=episode_data[2])
+                           series_name=episode_data[2], tables=[df.to_html(classes='data')], titles=df.columns.values,
+                           seg=[seg.to_html(classes='data')], times=seg.columns.values)
 
 
 if __name__ == '__main__':
--- influxclient.py
+++ influxclient.py
@@ -1,4 +1,5 @@
 from influxdb import DataFrameClient, InfluxDBClient
+import pandas as pd
 import json
 
 
@@ -16,7 +17,9 @@ def get_views(client: DataFrameClient, rp, measurement, resource, res_id, orga_i
         'FROM "{}"."{}" WHERE "organizationId"=$val1 AND "{}"=$val2 GROUP BY time(1d) FILL(0)'\
         .format(rp, measurement, resource)
     r = client.query(q, bind_params=params)
-    return r[measurement]
+    if r:
+        return r[measurement]
+    return pd.DataFrame()
 
 
 def get_segments(client: InfluxDBClient, rp, measurement, event_id, orga_id):
@@ -24,20 +27,13 @@ def get_segments(client: InfluxDBClient, rp, measurement, event_id, orga_id):
               'val2': event_id}
     q = 'SELECT "segments" FROM "{}"."{}" WHERE "organizationId"=$val1 AND "eventId"=$val2'.format(rp, measurement)
     r = client.query(q, bind_params=params)
-    point = list(r.get_points())[0]
-    j = json.loads(point['segments'])
-    result = []
-    for i in j:
-        result.append((i['nb_plays'], i['play_rate']))
-    return result
-
-
-c = DataFrameClient(database='opencast1')
-resul = get_views(c, 'autogen', 'impressions_daily', 'eventId', 'c667ea18-3239-479b-a940-3300607503eb', 'mh_default_org')
-print('Check data frame')
-print(resul)
-f = InfluxDBClient(database='opencast1')
-df = get_segments(f, 'autogen', 'segments_daily', 'c667ea18-3239-479b-a940-3300607503eb', 'mh_default_org')
-print("JSON")
-print(df)
-
+    points = list(r.get_points())
+    if points:
+        index = []
+        data = []
+        r_json = json.loads((points[0])['segments'])
+        for elem in r_json:
+            index.append(elem['label'])
+            data.append((elem['nb_plays'], elem['play_rate']))
+        return pd.DataFrame(data, columns=['plays', 'playrate'], index=index)
+    return pd.DataFrame()
