74d7283156e6e14c641519e501803bd36191b444
walsidalw <kleinfachnull@gmail.com>
2020-08-03 00:34:06 +0200

    Added improved scatter/lines plot for view data

--- app.py
+++ app.py
@@ -25,9 +25,10 @@ def index():
 
 @app.route('/org')
 def org():
-    df = influxclient.get_views(influx_clients['df_client'], rp, 'impressions_daily', 'organizationId', orgaId, orgaId)
-    graph = plots.get_plays_plot(df)
-    return render_template('org.html', tables=[df.to_html(classes='data')], titles=df.columns.values, graphJSON=graph)
+    plays_stats = influxclient.get_views(influx_clients['df_client'], rp, 'impressions_daily',
+                                         'organizationId', orgaId, orgaId)
+    graph = plots.get_plays_plot(plays_stats)
+    return render_template('org.html', graph=graph)
 
 
 @app.route('/series')
@@ -40,22 +41,23 @@ def series():
 def series_details(series_id):
     series_name = oc_client.get_series_name(series_id)
     episodes = oc_client.get_all_episodes(series_id)
-    df = influxclient.get_views(influx_clients['df_client'], rp, 'impressions_daily', 'seriesId', series_id, orgaId)
+    df = influxclient.get_views(influx_clients['df_client'], rp, 'impressions_daily', 'seriesId',
+                                series_id, orgaId)
     graph = plots.get_plays_plot(df)
     return render_template('series_details.html', series_id=series_id, series_name=series_name,
-                           episodes=episodes, tables=[df.to_html(classes='data')], titles=df.columns.values,
-                           graphJSON=graph)
+                           episodes=episodes, graph=graph)
 
 
 @app.route('/episodes/<episode_id>')
 def episode_details(episode_id):
     episode_data = oc_client.get_episode_data(episode_id)
-    df = influxclient.get_views(influx_clients['df_client'], rp, 'impressions_daily', 'eventId', episode_id, orgaId)
-    seg = influxclient.get_segments(influx_clients['point_client'], rp, 'segments_daily', episode_id, orgaId)
-    graphs = plots.get_plays_plot(df), plots.get_segments_plot(seg)
+    plays_stats = influxclient.get_views(influx_clients['df_client'], rp, 'impressions_daily',
+                                         'eventId', episode_id, orgaId)
+    segments_stats = influxclient.get_segments(influx_clients['point_client'], rp, 'segments_daily',
+                                               episode_id, orgaId)
+    graphs = plots.get_plays_plot(plays_stats), plots.get_segments_plot(segments_stats)
     return render_template('episode_details.html', episode_name=episode_data[0], series_id=episode_data[1],
-                           series_name=episode_data[2], tables=[df.to_html(classes='data')], titles=df.columns.values,
-                           seg=[seg.to_html(classes='data')], times=seg.columns.values, graphJSON=graphs)
+                           series_name=episode_data[2], graphs=graphs)
 
 
 if __name__ == '__main__':
--- plots.py
+++ plots.py
@@ -6,18 +6,51 @@ pd.options.plotting.backend = "plotly"
 
 
 def get_plays_plot(df):
-    plot = df.plot()
-    return json.dumps(plot, cls=plotly.utils.PlotlyJSONEncoder)
+    index = df.index.to_pydatetime()
+
+    trace1 = go.Scatter(x=index, y=list(df['plays']), mode='lines+markers', name='Plays')
+    trace2 = go.Scatter(x=index, y=list(df['visitors']), mode='lines+markers', name='Visitors')
+    trace3 = go.Scatter(x=index, y=list(df['finishes']), mode='lines+markers', name='Finishes')
+    data = [trace1, trace2, trace3]
+
+    layout = dict(
+        xaxis=dict(
+            showline=True,
+            showticklabels=True,
+            linecolor='rgb(204, 204, 204)',
+            linewidth=2,
+            ticks='outside',
+            tickfont=dict(
+                family='Arial',
+                size=12,
+                color='rgb(82, 82, 82)'),
+            showgrid=True,
+            gridcolor="#e6e6e6"),
+        yaxis=dict(
+            showgrid=True,
+            gridcolor="#e6e6e6",
+            zeroline=True,
+            zerolinecolor="#e6e6e6",
+            zerolinewidth=1),
+        plot_bgcolor='white',
+        margin=dict(l=30, r=0, pad=0, b=40, t=40))
+
+    fig = dict(data=data, layout=layout)
+    return json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
 
 
 def get_segments_plot(tup):
     x, y = tup
-    htemp = "<b>Zeitpunkt:</b> %{x}<br><b>Abspielrate:</b> %{z}<extra></extra>"
-    xaxis = {'nticks': 7, 'tickmode': 'auto', 'tickangle': 45, 'ticklen': 5, 'tickwidth': 1, 'tickcolor': '#444',
-             'ticks': 'outside'}
-    yaxis = {'showticklabels': False}
+    # Data and layout settings for heatmap plot
+    title = dict(text="Segmente")
+    colbar = dict(title=dict(text="Abspielrate", side="top"), x=1, y=0.515, ypad=0, len=1.03)
+    xaxis = dict(title=title, nticks=7, tickmode="auto", tickangle=45, ticklen=5, tickwidth=1,
+                 tickcolor="#444", ticks="outside")
+    yaxis = dict(showticklabels=False)
     marg = dict(l=20, r=20, pad=0, b=20, t=50)
-    fig = go.Figure(data=go.Heatmap(z=[y], x=x, colorscale='jet', zmin=0, zmax=max(y), zsmooth='fast',
-                    hovertemplate=htemp))
+    htemp = "<b>Zeitpunkt:</b> %{x}<br><b>Abspielrate:</b> %{z}<extra></extra>"
+    # Creation of heatmap plot
+    fig = go.Figure(data=go.Heatmap(z=[y], x=x, colorscale="jet", zmin=0, zmax=max(y), zsmooth="fast",
+                    hovertemplate=htemp, colorbar=colbar))
     fig.update_layout(xaxis=xaxis, yaxis=yaxis, showlegend=False, autosize=True, margin=marg)
     return json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
