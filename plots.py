762f18dc463fe30df4ad99b4ec3c687ee8d4c7da
walsidalw <kleinfachnull@gmail.com>
2020-08-03 16:27:36 +0200

    Formatting

--- plots.py
+++ plots.py
@@ -11,6 +11,7 @@ def get_plays_plot(df):
     trace1 = go.Scatter(x=index, y=list(df['plays']), mode='lines+markers', name='Plays')
     trace2 = go.Scatter(x=index, y=list(df['visitors']), mode='lines+markers', name='Visitors')
     trace3 = go.Scatter(x=index, y=list(df['finishes']), mode='lines+markers', name='Finishes')
+
     data = [trace1, trace2, trace3]
 
     layout = dict(
@@ -33,7 +34,12 @@ def get_plays_plot(df):
             zerolinecolor="#e6e6e6",
             zerolinewidth=1),
         plot_bgcolor='white',
-        margin=dict(l=30, r=0, pad=0, b=40, t=40))
+        margin=dict(
+            l=30,
+            r=0,
+            pad=0,
+            b=40,
+            t=40))
 
     fig = dict(data=data, layout=layout)
     return json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
@@ -41,39 +47,112 @@ def get_plays_plot(df):
 
 def get_segments_plot(tup):
     x, y = tup
-    # Data and layout settings for heatmap plot
-    title = dict(text="Segmente")
-    colbar = dict(title=dict(text="Abspielrate", side="top"), x=1, y=0.515, ypad=0, len=1.03)
-    xaxis = dict(title=title, nticks=7, tickmode="auto", tickangle=45, ticklen=5, tickwidth=1,
-                 tickcolor="#444", ticks="outside")
-    yaxis = dict(showticklabels=False)
-    marg = dict(l=20, r=20, pad=0, b=20, t=50)
-    htemp = "<b>Zeitpunkt:</b> %{x}<br><b>Abspielrate:</b> %{z}<extra></extra>"
-    # Creation of heatmap plot
-    fig = go.Figure(data=go.Heatmap(z=[y], x=x, colorscale="jet", zmin=0, zmax=max(y), zsmooth="fast",
-                    hovertemplate=htemp, colorbar=colbar))
-    fig.update_layout(xaxis=xaxis, yaxis=yaxis, showlegend=False, autosize=True, margin=marg)
+
+    data = go.Heatmap(dict(
+        z=[y],
+        x=x,
+        colorscale="jet",
+        zmin=0,
+        zmax=max(y),
+        zsmooth="fast",
+        colorbar=dict(
+            title=dict(
+                text="Abspielrate",
+                side="top"),
+            x=1,
+            y=0.515,
+            ypad=0,
+            len=1.03),
+        hovertemplate="<b>Zeitpunkt:</b> %{x}<br>"
+                      "<b>Abspielrate:</b> %{z}"
+                      "<extra></extra>"))
+
+    layout = dict(
+        showlegend=False,
+        autosize=True,
+        xaxis=dict(
+            title=dict(
+                text="Segmente"),
+            nticks=7,
+            tickmode="auto",
+            tickangle=45,
+            ticklen=5,
+            tickwidth=1,
+            tickcolor="#444",
+            ticks="outside"),
+        yaxis=dict(
+            showticklabels=False),
+        margin=dict(
+            l=20,
+            r=20,
+            pad=0,
+            b=20,
+            t=50))
+
+    fig = go.Figure(dict(data=data, layout=layout))
     return json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
 
 
 def get_bar_plot(tup):
     x, y = tup
+
     trace1 = go.Bar(x=x, y=[x[0] for x in y], name='Plays')
     trace2 = go.Bar(x=x, y=[x[1] for x in y], name='Visitors')
     trace3 = go.Bar(x=x, y=[x[2] for x in y], name='Finishes')
+
     data = [trace1, trace2, trace3]
+
     layout = dict(
         barmode='group',
-        bargap=0.15,
-        bargroupgap=0.1,
+        bargap=0.12,
+        bargroupgap=0.05,
         plot_bgcolor='white',
-        margin=dict(l=35, r=250, pad=0, b=250, t=40)
-    )
+        margin=dict(
+            l=35,
+            r=250,
+            pad=0,
+            b=250,
+            t=40))
+
     fig = dict(data=data, layout=layout)
     return json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
 
 
 def get_heatmap_plot(comb, events):
     index = comb[0].to_pydatetime()
-    fig = go.Figure(go.Heatmap(x=index, y=[x[1] for x in events], z=comb[1], colorscale='portland'))
+
+    data = go.Heatmap(dict(
+        x=index,
+        y=[x[1] for x in events],
+        z=comb[1],
+        colorscale='portland',
+        colorbar=dict(
+            title=dict(
+                text="Anzahl Besucher",
+                side="top"),
+            x=1,
+            y=0.515,
+            ypad=0,
+            len=1.03),
+        hovertemplate="<b>Datum:</b> %{x}<br>"
+                      "<b>Aufnahme:</b> %{y}<br>"
+                      "<b>Anzahl:</b> %{z}"
+                      "<extra></extra>"))
+
+    layout = dict(
+        autosize=True,
+        xaxis=dict(
+            tickmode="auto",
+            ticklen=5,
+            tickwidth=1,
+            tickcolor="#444",
+            ticks="outside"),
+        margin=dict(
+            l=0,
+            r=0,
+            pad=0,
+            b=20,
+            t=40))
+
+    fig = go.Figure(dict(data=data, layout=layout))
     return json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
